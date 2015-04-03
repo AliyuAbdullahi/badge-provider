69d20a3b2e205cbdfa168adeb177cfec5b343643
Aliyu Abdullahi <aliyuabdullahi@Andelas-MacBook-Pro-3.local>
2015-04-03 19:22:02 +0100

    Test database on MongoLab

--- config/env/development.js
+++ config/env/development.js
@@ -1,4 +1,5 @@
 module.exports = {
-  db: 'mongodb://localhost/badge-provider'
+  //db: 'mongodb://localhost/badge-provider'
+  db: 'mongodb://badge-provider:allahumasedika12@ds045757.mongolab.com:45757/badge-provider'
  // Development configuration options
 };
