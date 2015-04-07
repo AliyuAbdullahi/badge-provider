03f795696f06ad33e8f7cbc188792fbbfa32f94e
Aliyu Abdullahi <aliyuabdullahi@Andelas-MacBook-Pro-3.local>
2015-04-07 11:09:10 +0100

    Add nodemon

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ var mongoose = require('./config/mongoose'),
    express = require('./config/express');
 var db = mongoose();
 var app = express();
-
-app.listen(3000);
+app.set('port', (process.env.PORT || 5000));
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
 module.exports = app;
-console.log('Server running at http://localhost:3000/');
