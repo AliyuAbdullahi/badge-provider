e6c11016f3efb5f8a96c8c9b16c7133d230b1d5c
Aliyu Abdullahi <aliyuabdullahi@Andelas-MacBook-Pro-3.local>
2015-04-23 16:17:33 +0100

    modify mongoose for production

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,13 +1,13 @@
 var config = require('./config'),
  mongoose = require('mongoose');
 module.exports = function() {
-//var db = mongoose.connect(config.db.production.uri);
- var db = mongoose.connect(config.db[process.env.NODE_ENV].uri, config.db[process.env.NODE_ENV].options, function (err) {
-    if (err) {
-        console.error('Could not connect to MongoDB.');
-        console.log(err);
-    }
-});
+var db = mongoose.connect(config.db.production.uri);
+//  var db = mongoose.connect(config.db[process.env.NODE_ENV].uri, config.db[process.env.NODE_ENV].options, function (err) {
+//     if (err) {
+//         console.error('Could not connect to MongoDB.');
+//         console.log(err);
+//     }
+// });
  require('../app/features/models/badge.server.model');
  return db;
 };
