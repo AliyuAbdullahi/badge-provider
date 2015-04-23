034a021f903957b1b9925c99d3928721a2d2011b
Aliyu Abdullahi <aliyuabdullahi@Andelas-MacBook-Pro-3.local>
2015-04-23 23:40:43 +0100

    modify the model data by making them required

--- app/features/models/badge.server.model.js
+++ app/features/models/badge.server.model.js
@@ -1,8 +1,8 @@
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 var BadgeSchema = new Schema({
-    badge_title: {type: String},
-    badge_type:{type: String},
-    total:{type:Number,}
+    badge_title: {type: String, required:true},
+    badge_type:{type: String, required:true},
+    total:{type:Number, required:true}
 });
 mongoose.model('Badge', BadgeSchema);
